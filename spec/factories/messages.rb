8a3fafa4d222c20ed66aa61ab8b81910781c1b7c
kohta-rainy <artgc4663@gmail.com>
2021-07-03 14:37:26 +0900

    見直し

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
-    factory :message do
-      content {Faker::Lorem.sentence}
-      association :user
-      association :room
+  factory :message do
+    content {Faker::Lorem.sentence}
+    association :user
+    association :room
 
-      after(:build) do |message|
-        message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-      end
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
+end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'ユーザーログイン機能', type: :system do
-  it 'ログインしていない状態でトップページにアクセスした場合、サインインページに移動する' do
+  it 'ログインしていない場合、サインインページに移動する' do
     # トップページに遷移する
     visit root_path
 
